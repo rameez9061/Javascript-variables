d9bd9879de837eba93ca63042273146177771caa
Rameez Aziz <122540665+rameez9061@users.noreply.github.com>
2024-04-20 11:56:26 +0500

    Add files via upload
    This repo is about the variables and its rules in javascript .

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("This repo is for learning about the variables in javascript");
+let a =45;
+console.log(a);
+
+//variables can only be started with $ letters , digits , and underscores
+let $name = "Rameez";
+console.log($name);
+
+// variables cannnot be start with numbers it will throw an error
+
+let 5name = "Arshad";
+// This code will give a syntax error
+
+// Another rule is that javascript reserved keywords cannot be used as javascript variables 
+
+let var = 5;
+// this will give syntax error because var is a reserved keyword in javascript
+
+// last one is that javascript is a case sensitive language 
+let harry  = "boy";
+let Harry = "man";
+console.log(harry);
+console.log(Harry);
+// both keywords are same for us but different for javascript 
